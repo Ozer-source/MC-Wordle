9dd60044bb9641e4618ac096dd7915f47a058d3a
unknown <ozery7679@gmail.com>
2025-11-11 10:27:49 +0100

    basic logica toegevoegt

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-creeper = {'release': 'Pre-Alpha', 'health': '20', 'height': '1.7', 'behavior': 'Hostile'}
+creeper = { 'name': 'creeper', 'release': 'Pre-Alpha', 'health': '20', 'height': '1.7', 'behavior': 'Hostile'};
+zombie = { 'name': 'zombie', 'release': 'Pre-Alpha', 'health': '20', 'height': '1.95', 'behavior': 'Hostile'};
+
+mobs = [creeper, zombie];
+mob = {};
+function StartGame() {
+    mob = mobs[Math.floor(Math.random() * mobs.length)];
+    console.log(mob.name);
+}
+
+
+function CheckInput() {
+    let input = document.querySelector('input').value;
+    input = input.toLowerCase();
+    if (input == 'creeper') {
+        for (let i = 0; i < 4; i++) {
+            document.querySelector('.box-' + (i + 1)).innerHTML = Object.values(mobs[0])[i];
+        }
+    }
+    else if (input == 'zombie') {
+        for (let i = 0; i < 4; i++) {
+            document.querySelector('.box-' + (i + 1)).innerHTML = Object.values(mobs[1])[i];
+        }
+    }
+
+
+    if (input == mob.name) {
+        alert('You guessed it right! It is ' + mob.name);
+        mob = {};
+    }
+}
+
+if (mob == {}) {
+    StartGame();
+}
